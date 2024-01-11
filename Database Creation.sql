bce2444ae10a0a026ba5799c5db9e95e90ec59fa
JonRMurphy <jonathan.murphy05@gmail.com>
2024-01-10 19:37:23 -0500

    Updated CSV and SQL file

---
+++ Database Creation.sql
@@ -0,0 +1,21 @@
+CREATE TABLE  imdb_top250_movies (
+    Num int  ,
+    Title varchar   ,
+    Year int  ,
+    Released date   ,
+    Runtime varchar  ,
+    Genre varchar   ,
+    Director varchar  ,
+    Writer varchar   ,
+    Actors varchar   ,
+    Plot varchar   ,
+    Language varchar   ,
+    Country varchar   ,
+    Awards varchar  ,
+    Metascore int   ,
+    imdbRating float4   ,
+    imdbVotes varchar  ,
+    Production varchar  
+);
+
+select * from imdb_top250_movies;
